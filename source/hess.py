b97eb518c330453ba411170b8df1925d565b6a01
KMicha <michael.kahle@yahoo.de>
2021-06-08 14:36:56 +0200

    creation of daily gwl csv file

--- source/hess.py
+++ source/hess.py
@@ -6,18 +6,25 @@ import math
 import codecs
 import io
 
-fileName='./csv/hess_monthly.csv'
+fileName='./raw/hess_daily.csv'
 
 months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
 gwls = ['BM','HB','HFA','HFZ','HM','HNA','HNFA','HNFZ','HNZ','NA','NEA','NEZ','NWA','NWZ','NZ','SA','SEA','SEZ','SWA','SWZ','SZ','TB','TM','TRM','TRW','U','WA','WS','WW','WZ',]
 #months = ['jan']
-baurData = {}
+
+
+
+csvfile2 = codecs.open(fileName, "w", "utf-8")
+csvfile2.write("year,month,day,gwl\n")
+
 
 m = 0
 for month in months:
     m += 1
     fileName1 = './raw/'+month+'.txt'
     with open(fileName1, "r") as ins:
+        d = 0 
+        year = 0
         for line in ins:
             #array.append(line)
             words = line.split(' ')
@@ -26,13 +33,16 @@ for month in months:
                     empty = 1 
                 elif(word.isdigit()):
                     if(int(word)>32): 
-                        if(len(word)>4):   
-                            print(month)
-                            print(word)
-                    gigit = 1
+                        if(len(word)==4):   
+                            year = int(word)
+                            d = 0
                 elif(word in gwls):
                     gwl = 1
+                    d += 1
+                    #print([year,m,word])
+                    csvfile2.write(str(year)+","+str(m)+","+str(d)+","+word.strip()+"\n") 
                 else:
                     other = 1
                     #print(word)        
-                
+
+csvfile2.close()                  
